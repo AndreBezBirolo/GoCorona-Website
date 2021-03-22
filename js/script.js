6a0f197cdccfa369e1d3bff795c6ad620ddcfa0a
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-03-22 20:20:37 -0300

    Finalização do projeto GoCorona

--- js/script.js
+++ js/script.js
@@ -37,4 +37,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
   }
   activeCounter();
+
+  function toggleMenu() {
+    let button = document.querySelector(".burger-menu"),
+        overlay = document.querySelector(".overlay-menu"),
+        menu = document.querySelector("ul.container-menu");
+
+    button.addEventListener("click", function(){
+        overlay.classList.add("active");
+        menu.classList.add("active");
+    });
+
+    function hideMenu() {
+        overlay.classList.remove("active");
+        menu.classList.remove("active");
+    }
+
+    overlay.addEventListener("click", function(){
+        hideMenu();
+    });
+
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode === 27) {
+            hideMenu();
+        }
+    };
+
+}
+toggleMenu();
+
 });
